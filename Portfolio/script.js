510bfdeca002f220f24928dfb0c11be80b7a8927
Rajeshr0704 <143807893+Rajeshr0704@users.noreply.github.com>
2024-08-11 12:17:40 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menulist = document.getElementById("menu-list")
+    if(menulist.style.display ==='block'){
+        menulist.style.display ='none';
+    }
+    else{
+        menulist.style.display ='block'
+    }
+}
